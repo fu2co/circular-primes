7274365a92471ee087e6ace9d64007c2e508af2e
Jurrian Fahner <jurrian.fahner@gmail.com>
2024-03-19 22:15:49 +0100

    make test pass - There are 0 circular primes below 1.

--- src/main/java/nl/ensignprojects/katas/prime/CirclePrimeCounter.java
+++ src/main/java/nl/ensignprojects/katas/prime/CirclePrimeCounter.java
@@ -2,6 +2,10 @@ package nl.ensignprojects.katas.prime;
 
 public class CirclePrimeCounter {
     public int count(int number) {
+        if (number == 0 || number == 1) {
+            return 0;
+        }
+
         return 1;
     }
 }
